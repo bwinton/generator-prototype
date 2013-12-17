317d6410830e7106982cf121142c61e26d212fc1
Blake Winton <bwinton@latte.ca>
2013-12-16 22:29:59 -0500

    Add a task to build the XPI.

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -6,11 +6,24 @@ module.exports = function(grunt){
 
   grunt.loadNpmTasks('grunt-ssh');
 
-  var commands = [
-    ['cfx', 'xpi',
-      '--update-link', 'https://people.mozilla.com/~bwinton/<%= _.slugify(appname) %>/<%= _.slugify(appname) %>.xpi',
-      '--update-url', 'https://people.mozilla.com/~bwinton/<%= _.slugify(appname) %>/<%= _.slugify(appname) %>.update.rdf'],
-    [v, 'copyFile', 'whimsy.xpi', '/Volumes/people.mozilla.com/public_html/<%= _.slugify(appname) %>/<%= _.slugify(appname) %>.xpi'],
-    [v, 'copyFile', 'whimsy.update.rdf', '/Volumes/people.mozilla.com/public_html/<%= _.slugify(appname) %>/<%= _.slugify(appname) %>.update.rdf']
-  ];
+  grunt.registerTask('build', 'Build the add-on', function() {
+    var done = this.async();
+    grunt.util.spawn({
+      cmd: 'cfx',
+      args: ['xpi',
+        '--update-link',
+        'https://people.mozilla.com/~bwinton/<%= _.slugify(appname) %>/<%= _.slugify(appname) %>.xpi',
+        '--update-url',
+        'https://people.mozilla.com/~bwinton/<%= _.slugify(appname) %>/<%= _.slugify(appname) %>.update.rdf'
+      ]
+    }, function spawned(error, result, code) {
+      grunt.log.ok(result);
+      grunt.log.ok('Add-on built.');
+      var commands = [
+        ['copyFile', '<%= _.slugify(appname) %>.xpi', '/Volumes/people.mozilla.com/public_html/<%= _.slugify(appname) %>/<%= _.slugify(appname) %>.xpi'],
+        ['copyFile', '<%= _.slugify(appname) %>.update.rdf', '/Volumes/people.mozilla.com/public_html/<%= _.slugify(appname) %>/<%= _.slugify(appname) %>.update.rdf']
+      ];
+      done();
+    });
+  });
 };
